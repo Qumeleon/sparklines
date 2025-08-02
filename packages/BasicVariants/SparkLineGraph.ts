b918f608660cb20c4e369a947ba8c14833f849d9
Rene Buhrs <rbuhrs@gmail.com>
2025-08-02 12:18:02 +0200

    Adding doc for basic variants - line graph

--- packages/BasicVariants/SparkLineGraph.ts
+++ packages/BasicVariants/SparkLineGraph.ts
@@ -1,7 +1,25 @@
 import { IValues, SparkLines } from '../SparkLines'
 import { SparkLine } from './SparkLine.ts'
 
+/**
+ * Create a new SparkLines, as a line graph
+ *
+ */
 export class SparkLineGraph extends SparkLine {
+  /**
+   * Create a new SparkLines, as a line graph
+   *
+   * @param settings - Settings for the line graph
+   * @param settings.width - Width of the graph
+   * @param settings.height - Height of the graph
+   * @param settings.color - Color of the line, defaults to 'currentColor'
+   * @param settings.lineWidth - Width of the line
+   * @param settings.markers - Settings for the markers (dots) of the values on the graph
+   * @param settings.markers.color - Color of the markers, defaults to 'currentColor'
+   * @param settings.markers.size - Size of the markers
+   * @param values - Array of numeric values to use for the line graph
+   *
+   */
   constructor(
     settings: {
       width: number
